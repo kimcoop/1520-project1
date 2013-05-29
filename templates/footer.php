200f66fd8e6c44f7142bccbbbb3bb6139603db23
Kim Cooperrider <kim.cooperrider@gmail.com>
2013-05-29 16:24:17 -0400

    initial php scaffold

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+     -->
+
+  </body>
+</html>
