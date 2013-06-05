f4e478cee6be4fd4f3234e746c1433e3e27ef19b
Kim Cooperrider <kim.cooperrider@gmail.com>
2013-06-04 20:37:11 -0400

    initial scaffold for project info sheet at index.html

--- templates/header.php
+++ templates/header.php
@@ -15,16 +15,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <!-- Le styles -->
-    
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
-
     <link href="css/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-    <![endif]-->
     
   </head>
 
